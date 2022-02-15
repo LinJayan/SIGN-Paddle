85662bbb0e82f289642a421a57d9627c6e004168
LinJayan <43403833+LinJayan@users.noreply.github.com>
2022-02-15 12:46:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+# Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from dataset import SIGNDataset, random_split, collate_fn
 from net import SIGN
 import argparse
@@ -10,9 +24,9 @@ from train import train
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', type=str, default='frappe', help='which dataset to use')
 parser.add_argument('--dim', type=int, default=8, help='dimension of entity and relation embeddings')
-parser.add_argument('--l0_weight', type=float, default=0.001, help='weight of the l2 regularization term')
-parser.add_argument('--l2_weight', type=float, default=0.001, help='weight of the l2 regularization term')
-parser.add_argument('--lr', type=float, default=0.05, help='learning rate')
+parser.add_argument('--l0_weight', type=float, default=0.001, help='weight of the l2 regularization term') # 0.001  0.0002
+parser.add_argument('--l2_weight', type=float, default=0.001, help='weight of the l2 regularization term') # 0.001  0.0002
+parser.add_argument('--lr', type=float, default=0.05, help='learning rate') # 0.05  0.0015
 parser.add_argument('--batch_size', type=int, default=1024, help='batch size')
 parser.add_argument('--n_epoch', type=int, default=500, help='the number of epochs')
 parser.add_argument('--l0_para', nargs='?', default='[0.66, -0.1, 1.1]',
@@ -22,10 +36,11 @@ parser.add_argument('--hidden_layer', type=int, default=32, help='neural hidden 
 parser.add_argument('--pred_edges', type=int, default=1, help='!=0: use edges in dataset, 0: predict edges \
                                                                 using L_0')
 parser.add_argument('--random_seed', type=int, default=2019, help='size of common item be counted')
-parser.add_argument('--use_cuda',  action='store_true')
+parser.add_argument('--device', type=str, default='cpu', help='which dataset to use')
+# parser.add_argument('--use_cuda',  action='store_true')
 args = parser.parse_args()
 
-signdataset = SIGNDataset("./data/", "frappe", pred_edges=1)
+signdataset = SIGNDataset("./data/", args.dataset, pred_edges=args.pred_edges)
 
 data_num = signdataset.num_graph
 num_feature = signdataset.num_feature
